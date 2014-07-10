d019a67d7b5e02c96a5aa16a27b5dd6b2226f4e2
Work Paul <development1.paul@gmail.com>
2014-07-10 18:45:37 +0300

    removed flash code

--- web.rb
+++ web.rb
@@ -7,7 +7,6 @@ end
 set :public, "#{File.dirname(__FILE__)}/public"  
 
 get '/' do
-	flash[:blah] = "#{Time.now} - Please wait until the headless browser will finish the process."
 	load headless_test
   "Testing a simple headless browser on Heroku. You should see a screenshot bellow, based on <a href='http://www.timeanddate.com/worldclock/uk/london'>http://www.timeanddate.com/worldclock/uk/london</a> website. <br><img src='screenshot.png' style='
     width: 500px;
