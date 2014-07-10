a09409d18f6e8b60fc5621d80f0ee543cf4d24c0
Work Paul <development1.paul@gmail.com>
2014-07-10 18:39:14 +0300

    get screenshots on every page load

--- lib/google_headless.rb
+++ lib/google_headless.rb
@@ -2,6 +2,7 @@ require "watir-webdriver"
 
 @b = Watir::Browser.new :phantomjs
 @b.goto "http://www.timeanddate.com/worldclock/uk/london"
+
 @b.screenshot.save "public/screenshot.png"
 puts @b.title
 @b.close
--- web.rb
+++ web.rb
@@ -1,13 +1,15 @@
 require 'sinatra'
+require 'sinatra/flash'
 
 def headless_test 
-	"#{File.dirname(__FILE__)}/lib/google_headless"
+	"#{File.dirname(__FILE__)}/lib/google_headless.rb"
 end
 
 set :public, "#{File.dirname(__FILE__)}/public"  
 
 get '/' do
-	require headless_test 
+	flash[:blah] = "#{Time.now} - Please wait until the headless browser will finish the process."
+	load headless_test
   "Testing a simple headless browser on Heroku. You should see a screenshot bellow, based on <a href='http://www.timeanddate.com/worldclock/uk/london'>http://www.timeanddate.com/worldclock/uk/london</a> website. <br><img src='screenshot.png' style='
     width: 500px;
     border-image: inherit;
